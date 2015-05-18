950b12590a066cacd2d1044f8deac81679756786
student <student@mat301.umcs.lublin.pl>
2015-05-18 15:22:20 +0200

    inne komunikaty

--- src/jakasnowaapka/JakasNowaApka.java
+++ src/jakasnowaapka/JakasNowaApka.java
@@ -4,5 +4,10 @@ public class JakasNowaApka {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("bhjbhj");
+System.out.println("jhgjghj");
+System.out.println("biugygfhy");
+    
+    
     }
 }
